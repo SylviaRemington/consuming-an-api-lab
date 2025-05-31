1f6d8f053345e886855045a456d0524fff179ee8
SylviaRemington <sylviaremington@gmail.com>
2025-05-31 00:41:09 -0700

    addtl notes added

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
-//Getting further practice with this Level Up Exercise
+// Getting further practice with this Level Up Exercise
 // This is purely Level Up content & lab practice for when I have time.
+// This may or may not be completed. Originally, I thought I would have time to create this; 
+// but now, I'm realizing that I'll work on this as time allows.
 
