3604eaa61b5ed740d5126cfda8276f45ffc26685
SylviaRemington <sylviaremington@gmail.com>
2025-06-12 12:04:49 -0700

    adding notes

--- server.js
+++ server.js
@@ -3,3 +3,5 @@
 // This may or may not be completed. Originally, I thought I would have time to create this; 
 // but now, I'm realizing that I'll work on this as time allows.
 
+// CONSUMING AN API LAB PRACTICE
+
